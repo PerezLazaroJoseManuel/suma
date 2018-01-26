0635453c6bdbe8d7574364e0ad3c7d2f9e6af143
PérezLazaroJoseManuel <jmpela@hotmail.com>
2018-01-26 12:33:45 +0100

    SUMA
    Change-Id: I32341effe03ebcbfcb61befd39bb21d4625915d8
    Signed-off-by: PérezLazaroJoseManuel <jmpela@hotmail.com>

---
+++ src/suma/circulo.java
@@ -0,0 +1,11 @@
+package suma;
+
+public class circulo {
+
+	public static void main(String[] args) {
+		final float pi=3.1416F;
+		float r=5.2F;
+		System.out.println("El area del circulo es " + r*r*pi + " cm²");
+	}
+
+}
